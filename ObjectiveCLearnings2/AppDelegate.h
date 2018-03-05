c0cb25116ce884bd94813e19fd8da6529fef5c16
user <user@contus-lt-44.local>
2018-03-05 17:26:21 +0530

    Initial Commit

---
+++ ObjectiveCLearnings2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjectiveCLearnings2
+//
+//  Created by user on 05/03/18.
+//  Copyright © 2018 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjectiveCLearnings2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjectiveCLearnings2
+//
+//  Created by user on 05/03/18.
+//  Copyright © 2018 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
